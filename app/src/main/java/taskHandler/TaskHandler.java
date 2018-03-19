89f9c89bdd6d95903783c30a7a6bc183c6505318
Nepomuk1999 <christoph.bauer@students.fhv.at>
2018-03-19 12:49:52 +0100

    setup fix

---
+++ app/src/main/java/taskHandler/TaskHandler.java
@@ -0,0 +1,35 @@
+package taskHandler;
+
+/**
+ * Created by Home on 19.03.2018.
+ */
+
+public class TaskHandler {
+
+    private TaskHandler instance;
+
+    public TaskHandler getInstance(){
+        if (instance != null){
+            return instance;
+        } else {
+            instance = new TaskHandler();
+            return instance;
+        }
+    }
+
+    //TODO implement different tasks
+    public String getTaskAsString(){
+        return "1+1=";
+    }
+
+    //TODO implement real check
+    public boolean checkResult(String result){
+        if (result == "2"){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
