6254e392a7b57408a462cbcc375fdc50aa2aed8a
Nepomuk1999 <christoph.bauer@students.fhv.at>
2018-03-26 11:18:12 +0200

    Database integration

--- app/src/main/java/com/example/home/annoyingtaskalarm/MainActivity.java
+++ app/src/main/java/com/example/home/annoyingtaskalarm/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.home.annoyingtaskalarm;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import persistence.AnnoyingTaskAlarmDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AnnoyingTaskAlarmDatabase annoyingTaskAlarmDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        annoyingTaskAlarmDatabase = AnnoyingTaskAlarmDatabase.getInstance(this);
     }
 }
--- app/src/main/java/persistence/AnnoyingTaskAlarmDatabase.java
+++ app/src/main/java/persistence/AnnoyingTaskAlarmDatabase.java
@@ -1,7 +1,9 @@
 package persistence;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 /**
  * Created by Christoph Bauer on 26.03.2018.
@@ -17,5 +19,17 @@ import android.arch.persistence.room.RoomDatabase;
 @Database(entities = {Task.class}, version = 1)
 public abstract class AnnoyingTaskAlarmDatabase extends RoomDatabase {
 
+    private static AnnoyingTaskAlarmDatabase INSTANCE;
+
     public abstract TaskDao taskDao();
+
+    public static AnnoyingTaskAlarmDatabase getInstance(Context context){
+        if (INSTANCE == null){
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                    AnnoyingTaskAlarmDatabase.class,
+                    "AnnoyingTaskAlarmDatabase").build();
+        }
+        return INSTANCE;
+    }
+
 }
