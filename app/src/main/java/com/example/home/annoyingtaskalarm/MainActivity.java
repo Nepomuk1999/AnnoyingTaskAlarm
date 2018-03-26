cec905f74b6d3ea8d8487efa7c8931589f3884ac
smithY1337 <christopher.schmidt@students.fhv.at>
2018-03-26 11:19:36 +0200

    Code Ok Emu not Ok
    Tests

--- app/src/main/java/com/example/home/annoyingtaskalarm/MainActivity.java
+++ app/src/main/java/com/example/home/annoyingtaskalarm/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.home.annoyingtaskalarm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.concretepage.android.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
