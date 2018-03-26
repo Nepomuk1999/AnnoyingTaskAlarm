402b5c7a0b0f4ec2760de9a61262f4e331a6a470
Nepomuk1999 <christoph.bauer@students.fhv.at>
2018-03-26 12:53:36 +0200

    insert tasks

--- app/src/main/java/com/example/home/annoyingtaskalarm/MainActivity.java
+++ app/src/main/java/com/example/home/annoyingtaskalarm/MainActivity.java
@@ -3,16 +3,25 @@ package com.example.home.annoyingtaskalarm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import persistence.AnnoyingTaskAlarmDatabase;
+import taskHandler.TaskHandler;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean appPropperlyClosed;
+    private TaskHandler taskHandler;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        taskHandler.getInstance();
+        taskHandler.getAllTasks(this);
+
+
+
     }
 
     @Override
