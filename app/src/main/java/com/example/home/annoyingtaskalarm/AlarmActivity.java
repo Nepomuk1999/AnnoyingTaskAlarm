46212afef3872d2c4de1953bb94cd41210be01d7
zacki01 <mza3087@students.fhv.at>
2018-04-23 09:42:05 +0200

    - uncommented OnClickListener - added an ArrayList to provide multiple alarms

--- app/src/main/java/com/example/home/annoyingtaskalarm/AlarmActivity.java
+++ app/src/main/java/com/example/home/annoyingtaskalarm/AlarmActivity.java
@@ -1,5 +1,6 @@
 package com.example.home.annoyingtaskalarm;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -7,11 +8,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.SystemClock;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 
 public class AlarmActivity extends FragmentActivity{
@@ -37,37 +41,52 @@ public class AlarmActivity extends FragmentActivity{
         curTime = String.format("%02d:%02d", timeHour, timeMinute);
         alarmTime.setText(curTime);
 
+
         textView2 = (TextView)findViewById(R.id.msg2);
 
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+//        ArrayList<PendingIntent> intentArray = new ArrayList<PendingIntent>();
+//
+//        for(int i = 0; i < 7; ++i)
+//        {
+//            Intent intent = new Intent(AlarmActivity.this, AlarmReceiver.class);
+//            // Loop counter `i` is used as a `requestCode`
+//            PendingIntent pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, i, intent, 0);
+//            // Single alarms in 1, 2, ..., 10 minutes (in `i` minutes)
+//            alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+//                    SystemClock.elapsedRealtime() + 60000 * i,
+//                    pendingIntent);
+//
+//            intentArray.add(pendingIntent);
+//        }
         Intent intent = new Intent(AlarmActivity.this, AlarmReceiver.class);
         pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 0, intent, 0);
 
-//        OnClickListener listener1 = new OnClickListener() {
-//            public void onClick(View view) {
-//                textView2.setText("");
-//                Bundle bundle = new Bundle();
-//                bundle.putInt(MyAlarmConstants.HOUR, timeHour);
-//                bundle.putInt(MyAlarmConstants.MINUTE, timeMinute);
-//                MyDialogFragment fragment = new MyDialogFragment(new MyHandler());
-//                fragment.setArguments(bundle);
-//                FragmentManager manager = getSupportFragmentManager();
-//                FragmentTransaction transaction = manager.beginTransaction();
-//                transaction.add(fragment, MyAlarmConstants.TIME_PICKER);
-//                transaction.commit();
-//            }
-//        };
+        OnClickListener listener1 = new OnClickListener() {
+            public void onClick(View view) {
+                textView2.setText("");
+                Bundle bundle = new Bundle();
+                bundle.putInt(MyAlarmConstants.HOUR, timeHour);
+                bundle.putInt(MyAlarmConstants.MINUTE, timeMinute);
+                MyDialogFragment fragment = new MyDialogFragment(new MyHandler());
+                fragment.setArguments(bundle);
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.add(fragment, MyAlarmConstants.TIME_PICKER);
+                transaction.commit();
+            }
+        };
 
-//        Button btnSetAlarmTime = (Button)findViewById(R.id.btnSetAlarmTime);
-//        btnSetAlarmTime.setOnClickListener(listener1);
-//        OnClickListener listener2 = new OnClickListener() {
-//            public void onClick(View view) {
-//                textView2.setText("");
-//                cancelAlarm();
-//            }
-//        };
-//        Button saveAlarm = (Button)findViewById(R.id.saveAlarm);
-//        saveAlarm.setOnClickListener(listener2);
+        Button btnSetAlarmTime = (Button)findViewById(R.id.btnSetAlarmTime);
+        btnSetAlarmTime.setOnClickListener(listener1);
+       OnClickListener listener2 = new OnClickListener() {
+            public void onClick(View view) {
+                textView2.setText("");
+                cancelAlarm();
+            }
+        };
+        Button saveAlarm = (Button)findViewById(R.id.saveAlarm);
+        saveAlarm.setOnClickListener(listener2);
 
 
     }
@@ -89,15 +108,17 @@ public class AlarmActivity extends FragmentActivity{
     public void saveAlarmToList(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+        //System.out.println(alarmManager);
+        //alarmManager.cancel(pendingIntent);
     }
 
     // delete alarm
     public void deleteAlarmFromList(View view) {
-        textView2.setText("");
-        cancelAlarm();
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        //alarmManager.
+//        textView2.setText("");
+//        cancelAlarm();
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+
     }
 
     class MyHandler extends Handler {
--- app/src/main/java/com/example/home/annoyingtaskalarm/AlarmReceiver.java
+++ app/src/main/java/com/example/home/annoyingtaskalarm/AlarmReceiver.java
@@ -10,6 +10,7 @@ import android.support.v4.content.WakefulBroadcastReceiver;
 public class AlarmReceiver extends WakefulBroadcastReceiver{
 
     public void onReceive(final Context context, Intent intent) {
+        // POP-UP HERE!!!!!!!!!
         AlarmActivity.getTextView2().setText("THIS IS A TEST!");
         Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         Ringtone ringtone = RingtoneManager.getRingtone(context, uri);
