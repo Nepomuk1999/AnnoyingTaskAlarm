b9479d9359f06ac94f5729ffa387f3f37c4591fb
smithY1337 <christopher.schmidt@students.fhv.at>
2018-05-07 11:10:56 +0200

    Battery Level

---
+++ app/src/main/java/com/example/home/annoyingtaskalarm/BatteryLevelReceiver.java
@@ -0,0 +1,17 @@
+package com.example.home.annoyingtaskalarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class BatteryLevelReceiver extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "Battery Level Low!", Toast.LENGTH_LONG).show();
+        Log.e("", "BATTERY LOW!");
+
+    }
+}
