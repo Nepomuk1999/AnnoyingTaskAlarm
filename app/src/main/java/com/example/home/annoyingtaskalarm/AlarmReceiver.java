d260d7454a88da5153e17b7f30ada3cc9486d6fc
zacki01 <mza3087@students.fhv.at>
2018-05-09 23:10:09 +0200

    unused imports deleted

--- app/src/main/java/com/example/home/annoyingtaskalarm/AlarmReceiver.java
+++ app/src/main/java/com/example/home/annoyingtaskalarm/AlarmReceiver.java
@@ -1,14 +1,11 @@
 package com.example.home.annoyingtaskalarm;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.v4.content.WakefulBroadcastReceiver;
-import android.text.method.ArrowKeyMovementMethod;
 
 import handler.TaskHandler;
 
