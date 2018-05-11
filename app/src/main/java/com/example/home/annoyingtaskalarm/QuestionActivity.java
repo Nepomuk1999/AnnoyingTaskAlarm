2bce1307032d0e55dbdc3bc3c5dfcf0030510472
zacki01 <mza3087@students.fhv.at>
2018-05-11 10:46:54 +0200

    on create part fixed

--- app/src/main/java/com/example/home/annoyingtaskalarm/QuestionActivity.java
+++ app/src/main/java/com/example/home/annoyingtaskalarm/QuestionActivity.java
@@ -2,6 +2,7 @@ package com.example.home.annoyingtaskalarm;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -9,6 +10,7 @@ public class QuestionActivity extends AppCompatActivity {
 
     private EditText editTextAnswer;
     private TextView showQuestion;
+    private Button btnAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +18,19 @@ public class QuestionActivity extends AppCompatActivity {
 
         setContentView(R.layout.popup_dialog);
         System.out.println("in QuestionOnCreate!!!!");
+
+        editTextAnswer = (EditText) findViewById(R.id.alarmAnswer);
+        showQuestion = (TextView) findViewById(R.id.alarmQuestion);
+
+        btnAnswer = (Button) findViewById(R.id.btnQuestion);
+
+        // get Alarmentity and insert Question here!!!
+        //showQuestion.setText();
+
+        String answer = editTextAnswer.getText().toString();
+
+
+
+        // check answer with question answer
     }
 }
