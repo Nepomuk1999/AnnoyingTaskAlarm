835399179e9e788a6aa6ec2c8298056911632edd
zacki01 <mza3087@students.fhv.at>
2018-05-11 10:54:51 +0200

    fixed

--- app/src/main/java/com/example/home/annoyingtaskalarm/QuestionActivity.java
+++ app/src/main/java/com/example/home/annoyingtaskalarm/QuestionActivity.java
@@ -1,10 +1,14 @@
 package com.example.home.annoyingtaskalarm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import handler.AlarmHandler;
 
 public class QuestionActivity extends AppCompatActivity {
 
@@ -21,20 +25,29 @@ public class QuestionActivity extends AppCompatActivity {
 
         editTextAnswer = (EditText) findViewById(R.id.alarmAnswer);
         showQuestion = (TextView) findViewById(R.id.alarmQuestion);
-
         btnAnswer = (Button) findViewById(R.id.btnQuestion);
+
+        // listener for check button
+        btnAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(view);
+            }
+        });
 
         // get Alarmentity and insert Question here!!!
         //showQuestion.setText();
 
-        String answer = editTextAnswer.getText().toString();
-
-
-
-        // check answer with question answer
     }
 
-    public void checkAnswer() {
+    public void checkAnswer(View view) {
+        // check answer with question answer
+        String answer = editTextAnswer.getText().toString();
 
+        //check anwer here!!!
+
+        // if ok then go back to main view
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
